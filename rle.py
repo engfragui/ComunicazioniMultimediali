38cdd493f82fd89368f8380482c8956927a2251b
Francesca Guiducci <guiducci.francesca@gmail.com>
2012-05-13 15:48:40 +0200

    RLE ora divide in due i run > 255 caratteri, proprio come da specifiche :)

--- rle.py
+++ rle.py
@@ -6,6 +6,12 @@ import time
 
 class rle:
 
+    def newstrout(self,counter,lastChar):
+
+        counter_bin = '%(#)08d' % {"#" : int(bin(counter)[2:])} #converto il counter in una stringa di 1 e 0
+        ba = bitarray(counter_bin) #dalla stringa ricavo l'oggetto bitarray
+        return ba.tobytes() + lastChar #scrivo il carattere precedente preceduto dal counter salvato come singolo byte
+
     def encode(self, file_input, file_output):
 
         f = open(file_input, 'r') #apro file di input in lettura
@@ -19,19 +25,16 @@ class rle:
 
             if lastChar == c: #se il carattere precedente e' uguale a quello attuale
                 counter += 1 #incremento il contatore
+                if counter==256: #quando ho letto 256 caratteri tutti uguali devo scrivere i primi 255 su file e memorizzare il 256esimo
+                    strout += self.newstrout(255,lastChar)
+                    counter = 1
 
             else: #se carattere precedente e' diverso da quello attuale
-                counter_bin = '%(#)08d' % {"#" : int(bin(counter)[2:])} #converto il counter in una stringa di 1 e 0
-                ba = bitarray(counter_bin) #dalla stringa ricavo l'oggetto bitarray
-                strout += ba.tobytes() + lastChar #scrivo il carattere precedente preceduto dal counter salvato come singolo byte
+                strout += self.newstrout(counter,lastChar)
                 lastChar = c #inizializzo il carattere attuale
                 counter = 1
 
-        #scrivo anche l'ultimo carattere
-        counter_bin = '%(#)08d' % {"#" : int(bin(counter)[2:])}
-        ba = bitarray(counter_bin)
-        strout += ba.tobytes() + lastChar
-
+        strout += self.newstrout(counter,lastChar) #scrivo anche l'ultimo carattere
         o.write(strout) #scrivo l'output in ASCII (in questo modo e' visibile) sul file_output
         o.close() #chiudo file_output
         f.close() #chiudo file_input
@@ -50,7 +53,12 @@ class rle:
             if state == 0: #state = 0 --> c e' il counter
                 ba = bitarray()
                 ba.frombytes(c) #dal byte mi ricavo l'oggetto bitarray
-                counter_int = int(binascii.b2a_hex(ba.tostring()),16) #converto prima in esadecimale e poi in decimale
+                if ba[0]: #ossia se il primo carattere della stringa di bit e' a 1 (numero > di 127) non posso convertire in string
+                    ba[0] = 0 #azzero il primo carattere
+                    counter_int = int(binascii.b2a_hex(ba.tostring()),16) #converto prima in esadecimale e poi in decimale
+                    counter_int += 128 #aggiungo 10000000(bin) = 128(dec) che avevo tolto all'inizio
+                else:
+                    counter_int = int(binascii.b2a_hex(ba.tostring()),16) #converto prima in esadecimale e poi in decimale
                 state = 1 #metto lo stato a 1
 
             else: #state = 1 --> c e' il carattere
